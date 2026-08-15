5ec143bc13b238cb33aa7be72e327f135991b038
Def1ce <zheng2007116@gmail.com>
2026-08-15 18:35:28 +0800

    chore: point to bin/dsh-bridge.cjs

--- start.bat
+++ start.bat
@@ -2,5 +2,5 @@
 REM dsh-lan-bridge - one-click start (double-click this file)
 REM Works no matter where this repo is extracted/run from.
 cd /d "%~dp0"
-node bin\dsh-bridge.js
+node bin\dsh-bridge.cjs
 pause
